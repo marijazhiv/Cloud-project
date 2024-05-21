ca3bb5fa01b4af49a5dc9f06c050781b13ee5fb6
marijazhiv <105136036+marijazhiv@users.noreply.github.com>
2024-05-21 12:15:25 +0200

    home page added

--- Cloud22/src/app/main-page/main-page.component.ts
+++ Cloud22/src/app/main-page/main-page.component.ts
@@ -5,7 +5,7 @@ import {RouterOutlet} from "@angular/router";
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
 
