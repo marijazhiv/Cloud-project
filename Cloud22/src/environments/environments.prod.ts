9e91b6d4290950c48b4f1ca7fd5d8122400702b8
bojangelic <bojangelic417@gmail.com>
2024-08-06 14:46:40 +0200

    Created environments files

---
+++ Cloud22/src/environments/environments.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    cognito: {
+        userPoolId: 'eu-central-1_qvepa1XN6',
+        userPoolWebClientId: '5grulmlme5rfo04q0euhds31c2'
+    }
+};git add 
---
+++ Cloud22/src/environments/environments.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    cognito: {
+        userPoolId: 'eu-central-1_qvepa1XN6',
+        userPoolWebClientId: '5grulmlme5rfo04q0euhds31c2'
+    }
+};
