8887376e7bc6d86100c5fd27f5d75c53b29b0812
bojangelic <bojangelic417@gmail.com>
2024-08-28 18:26:30 +0200

    Download movie

--- Cloud22/src/app/main-page/main-page.component.ts
+++ Cloud22/src/app/main-page/main-page.component.ts
@@ -1,41 +1,41 @@
-import {Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import {ReactiveFormsModule} from "@angular/forms";
-import {Router, RouterOutlet} from "@angular/router";
-import {environment} from "../environment/environment";
-import {CognitoUserPool} from "amazon-cognito-identity-js";
-
+import {RouterOutlet} from "@angular/router";
+import {concatMap, mergeMap} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Movie} from "./movie";
+import {FilmService} from "./film.service";
+import {S3Service} from "../s3.service";
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit{
-  user: any;
+export class MainPageComponent {
+  //videoUrl: string = "cloud.mkv";
+  //movie: Movie | undefined;
 
-  constructor(private router: Router) {
+  constructor(private s3Service: S3Service) {}
+
+  downloadFilm(bucketName: string, key: string): void {
+    this.s3Service.downloadFile(bucketName, key).subscribe({
+      next: (blob) => {
+        // Kreirajte URL za preuzimanje
+        const url = URL.createObjectURL(blob);
+
+        // Kreirajte privremeni link za preuzimanje
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = key; // Postavite naziv fajla koji želite da preuzmete
+        document.body.appendChild(a);
+        a.click();
+
+        // Očistite URL
+        URL.revokeObjectURL(url);
+      },
+      error: (err) => {
+        console.error('Error downloading file:', err);
+      }
+    });
   }
-
-  private userPoolData = {
-    UserPoolId: environment.cognito.userPoolId,
-    ClientId: environment.cognito.userPoolWebClientId
-  };
-
-  private userPool = new CognitoUserPool(this.userPoolData);
-
-  ngOnInit(): void {
-    this.user = JSON.parse(localStorage.getItem("user") || '')
-    console.log(this.user);
-  }
-
-  logOut(){
-    const cognitoUser = this.userPool.getCurrentUser();
-
-    if (cognitoUser) {
-      cognitoUser.signOut();
-    }
-
-    localStorage.clear();
-    this.router.navigate(['']);
-  }
-
 }
